252b9a60bb6a5a8d6574d874ff8f93b415b5e6ba
raddevus <1radeutsch1@gmail.com>
2020-10-04 13:32:35 -0400

    Complete working version.Provides favicon, works offline, implements responsive design and resizes properly on smart phone.

--- cyapass/serviceworker.js
+++ cyapass/serviceworker.js
@@ -1,4 +1,3 @@
-
 self.addEventListener('install', function(event) {
     event.waitUntil(
     caches.open('cya-cache').then(function(cache) {
@@ -7,24 +6,39 @@ self.addEventListener('install', function(event) {
             'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
             'https://code.jquery.com/jquery-2.2.3.min.js',
             'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js',
-            '/css/main.css',
-            '/js/cyapass.js',
-            '/js/sha256.js',
-            '/index.htm',
-            '/cyapass.ico',
-            '/cyapass.png'
+            'css/main.css',
+            'js/app.js',
+            'js/cyapass.js',
+            'js/sha256.js',
+            'offline.htm',
+            'cyapass.ico',
+            'cyapass.png'
+
         ]
         );
     })
     );
 });
 
-self.addEventListener('fetch', function(event) {
-    console.log("fetching...");
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("offline.htm");
+        }
+      });
+    })
+  );
+});
+// self.addEventListener('fetch', function(event) {
+//     console.log("fetching...");
+//     event.respondWith(
+//       caches.match(event.request).then(function(response) {
+//         return response || fetch(event.request);
+//       })
+//     );
+//   });
   
